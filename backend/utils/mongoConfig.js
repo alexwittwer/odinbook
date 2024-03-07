72ce13ac2c15dfa82c4266d7511417d445f5e1bf
alexwittwer <wittweral@gmail.com>
2024-03-07 16:23:44 -0500

    fix typo, change from IIFE

--- backend/utils/mongoConfig.js
+++ backend/utils/mongoConfig.js
@@ -3,16 +3,9 @@ require('dotenv').config();
 
 let bucket;
 
-(() => {
-  try {
-    const mongoDB = process.env.MONGODB_URI;
-    mongoose.connect(mongoDB);
-    const db = mongoose.connection;
-    bucket = new mongoose.mongo.GridFSBucket(db, {bucketName: photos})
-    db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
-    db.once("connect", () => console.log("Connected to MongoDB"));
-  } catch(err){
-    console.warn(err);
-  }
-
-})();
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+bucket = new mongoose.mongo.GridFSBucket(db, { bucketName: "Photo" })
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+db.once("connect", () => console.log("Connected to MongoDB"));
